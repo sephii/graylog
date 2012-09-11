cdbbc3926cbbfb39c7adfa09b87be0f0347b7710
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2012-09-11 14:16:32 +0200

    use our version of gelf to have the severity option

--- drupal7.sh
+++ drupal7.sh
@@ -4,7 +4,9 @@ test ! -e sites/all/modules/libraries && git submodule add --branch 7.x-2.x http
 drush pm-list --pipe --status='enabled'|grep -qx libraries || drush pm-enable -y libraries
 
 test ! -e sites/all/libraries/gelf-php && git submodule add https://github.com/Graylog2/gelf-php.git sites/all/libraries/gelf-php
-test ! -e sites/all/modules/gelf && git submodule add --branch 7.x-1.x http://git.drupal.org/project/gelf.git sites/all/modules/gelf
+# Use our version of the gelf module until http://drupal.org/node/1781116 gets
+# merged
+test ! -e sites/all/modules/gelf && git submodule add --branch 7.x-1.x https://github.com/sephii/gelf.git sites/all/modules/gelf
 drush pm-list --pipe --status='enabled'|grep -qx gelf || drush pm-enable -y gelf
 
 if [ -f modules.enabled ]; then
